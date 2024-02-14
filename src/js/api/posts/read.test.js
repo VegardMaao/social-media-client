f68f048e3cdad33c5fa7e07881cab36e94bae5f5
Vegard M. <vm932648@gmail.com>
2024-02-14 15:11:30 +0100

    Working on  unit  tests

---
+++ src/js/api/posts/read.test.js
@@ -0,0 +1,59 @@
+import { describe } from "yargs";
+import { getPosts } from "./read";
+
+const mockFetchSuccess = jest.fn().mockResolvedValue({
+  ok: true,
+  json: jest.fn().mockResolvedValue([
+    {
+      title: "string",
+      body: "string",
+      tags: ["string"],
+      media: "string",
+      reactions: [
+        {
+          symbol: "string",
+          count: 0,
+          postId: 0,
+        },
+      ],
+      comments: [
+        {
+          body: "string",
+          replyToId: 0,
+          id: 0,
+          postId: 0,
+          owner: "string",
+          created: "2024-02-14T13:58:27.483Z",
+          author: {
+            name: "string",
+            email: "user@example.com",
+            avatar: "string",
+            banner: "string",
+          },
+        },
+      ],
+      created: "2024-02-14T13:58:27.483Z",
+      updated: "2024-02-14T13:58:27.483Z",
+      id: 0,
+      author: {
+        name: "string",
+        email: "user@example.com",
+        avatar: "string",
+        banner: "string",
+      },
+      _count: {
+        comments: 0,
+        reactions: 0,
+      },
+    },
+  ]),
+});
+
+global.fetch = mockFetchSuccess;
+
+describe("getPosts", () => {
+  it("returns an array of posts", async () => {
+    const data = await getPosts();
+    expect(data).toContain(data.title);
+  });
+});
